324ee8099675f2987a53d09a27e7f42e717dcc47
Seka Diasnoriga Koswara <diasnorigakoswaraseka@gmail.com>
2025-08-04 17:29:48 +0700

    Comment out FooterWidget in HomePage for layout adjustments

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -57,7 +57,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                       MyProjectsWidget(),
                       TrustedCompanyWidget(),
                       AboutmeWidget(),
-                      FooterWidget(),
+                      // FooterWidget(),
                     ],
                   ),
                 ),
